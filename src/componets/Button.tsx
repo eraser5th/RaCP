40153016767c696dcfc4329c51d2f4c1d29a1994
eraser5th <hisanniwo0202@gmail.com>
2021-12-01 22:26:55 +0900

    ボタンとそのCSSを編集

--- src/componets/Button.tsx
+++ src/componets/Button.tsx
@@ -2,12 +2,24 @@ import React from 'react';
 import '../css/button.css';
 
 type ButtonProps = {
-  size: 'sm' | 'lg',
-  text: string
+  text: string,
+  size?: 'sm' | 'lg',
+  onClick?: () => void,
+  link?: string
 }
 
-const Button: React.VFC<ButtonProps> = ({ size = 'lg', text }) => (
-  <button type="button" className={`btn-${size}`}>{text}</button>
+const Button: React.VFC<ButtonProps> = ({
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  size = 'lg', text, onClick = () => {}, link = '#',
+}) => (
+  <button
+    type="button"
+    className={`btn btn-${size}`}
+    ref={link}
+    onClick={onClick}
+  >
+    {text}
+  </button>
 );
 
 export default Button;
