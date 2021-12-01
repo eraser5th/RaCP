48532519c16d1a0cdb92ae8e9c6a315db935df45
eraser5th <hisanniwo0202@gmail.com>
2021-12-01 20:49:16 +0900

    適当なボタンを作った

--- src/componets/Button.tsx
+++ src/componets/Button.tsx
@@ -1,1 +1,13 @@
 import React from 'react';
+import '../css/button.css';
+
+type ButtonProps = {
+  size: 'sm' | 'lg',
+  text: string
+}
+
+const Button: React.VFC<ButtonProps> = ({ size = 'lg', text }) => (
+  <button type="button" className={`btn-${size}`}>{text}</button>
+);
+
+export default Button;
