6d761bfee8bef71873a4c5e883bc255d5303de30
eraser5th <hisanniwo0202@gmail.com>
2021-12-04 01:53:58 +0900

    mainを適当に作った

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,37 @@
 import React from 'react';
-import Logo from './logo';
 import './App.css';
 import Header from './componets/Header';
 import Footer from './componets/Footer';
+import Card from './componets/Card';
 
 const App: React.VFC = () => (
   <div className="app">
     <Header links={['link1', 'link2']} />
-    <header className="app-header">
-      <Logo />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.tsx</code>
-        {' '}
-        and save to reload.
-      </p>
-      <a
-        className="app-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
+    <main className="main">
+      <Card rounded />
+      <Card rounded />
+      <Card rounded />
+      <Card rounded />
+      <Card rounded />
+      <Card rounded />
+      <Card rounded />
+      <Card rounded />
+      <Card rounded />
+      <Card rounded />
+      <Card rounded />
+      <Card rounded />
+      <Card rounded />
+      <Card rounded />
+      <Card rounded />
+      <Card rounded />
+      <Card rounded />
+      <Card rounded />
+      <Card rounded />
+      <Card rounded />
+      <Card rounded />
+      <Card rounded />
+      <Card rounded />
+    </main>
     <Footer links={['link1', 'link2']} />
   </div>
 );
