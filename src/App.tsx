7d5c31e47796cdcd3a388796f9f00aaf2b0e7324
eraser5th <hisanniwo0202@gmail.com>
2021-12-04 13:39:34 +0900

    cardsを切り分けて直した

--- src/App.tsx
+++ src/App.tsx
@@ -4,38 +4,22 @@ import Header from './componets/Header';
 import Footer from './componets/Footer';
 import Card from './componets/Card';
 
-const App: React.VFC = () => (
-  <div className="app">
-    <Header links={['link1', 'link2']} />
-    <main className="main">
-      <div className="main--inner">
-        <Card rounded />
-        <Card rounded />
-        <Card rounded />
-        <Card rounded />
-        <Card rounded />
-        <Card rounded />
-        <Card rounded />
-        <Card rounded />
-        <Card rounded />
-        <Card rounded />
-        <Card rounded />
-        <Card rounded />
-        <Card rounded />
-        <Card rounded />
-        <Card rounded />
-        <Card rounded />
-        <Card rounded />
-        <Card rounded />
-        <Card rounded />
-        <Card rounded />
-        <Card rounded />
-        <Card rounded />
-        <Card rounded />
-      </div>
-    </main>
-    <Footer links={['link1', 'link2']} />
-  </div>
-);
+const App: React.VFC = () => {
+  const length = 18;
+  return (
+    <div className="app">
+      <Header links={['link1', 'link2']} />
+      <main className="main">
+        <div className="main--inner">
+          <div className="main--cards">
+            {new Array(length).fill(0).map(() => <Card rounded />)}
+            {new Array(length % 4).fill(0).map(() => <div style={{ width: '300px', height: '300px' }} />)}
+          </div>
+        </div>
+      </main>
+      <Footer links={['link1', 'link2']} />
+    </div>
+  );
+};
 
 export default App;
