d9726e336706241aa666f2d4f6e7b31a0f9fb3cc
eraser5th <hisanniwo0202@gmail.com>
2021-12-04 21:42:43 +0900

    add rounded

--- src/componets/CardList.tsx
+++ src/componets/CardList.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Button from './Button';
 import Card from './Card';
 import './cardList.css';
+import ComponentViewCard from './ComponentViewCard';
 
 const CardList: React.VFC<{cardList: any[]}> = ({ cardList }) => (
   <div className="card-list">
-    {cardList.map(() => <Card title="test title" explain="test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text test text " rounded />)}
+    {cardList.map((item) => (
+      <ComponentViewCard>
+        <Button label="button" />
+      </ComponentViewCard>
+    ))}
   </div>
 );
 
