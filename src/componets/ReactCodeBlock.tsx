afee08e461ad6234847acfd69ae13f2315b1126e
eraser5th <hisanniwo0202@gmail.com>
2021-12-04 23:14:06 +0900

    change filename

--- src/componets/ReactCodeBlock.tsx
+++ src/componets/ReactCodeBlock.tsx
@@ -1,12 +1,26 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import React from 'react';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+const code = "import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';\n"
++ "import React from 'react';\n"
++ "import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';\n"
++ 'const ReactCodeBlock: React.VFC = () => {\n'
++ "  const codeString = '(num) => num + 1';\n"
++ '  return (\n'
++ '\n'
++ '      {codeString}\n'
++ '   </SyntaxHighlighter>\n'
++ '  );\n'
++ '};\n'
++ '\n'
++ 'export default ReactCodeBlock;\n';
 
 const ReactCodeBlock: React.VFC = () => {
   const codeString = '(num) => num + 1';
   return (
-    <SyntaxHighlighter language="javascript" style={dark}>
-      {codeString}
+    <SyntaxHighlighter language="tsx" style={vscDarkPlus}>
+      {code}
     </SyntaxHighlighter>
   );
 };
--- src/stories/ReactCodeBlock.stories.tsx
+++ src/stories/ReactCodeBlock.stories.tsx
@@ -4,18 +4,15 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import ReactCodeBlock from '../componets/ReactCodeBlock';
 
 export default {
-  title: 'Example/Card',
+  title: 'Example/CodeBlocks',
   component: ReactCodeBlock,
   argTypes: {
-    rounded: {
-      control: 'boolean',
-    },
   },
 } as ComponentMeta<typeof ReactCodeBlock>;
 
 // eslint-disable-next-line react/jsx-props-no-spreading
 const Template: ComponentStory<typeof ReactCodeBlock> = (args) => <ReactCodeBlock {...args} />;
 
-export const TestCard = Template.bind({});
-TestCard.args = {
+export const ReactCode = Template.bind({});
+ReactCode.args = {
 };
