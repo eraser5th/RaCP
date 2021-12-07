642bf0b79528746a8232c5d103de727f98d7339a
eraser5th <hisanniwo0202@gmail.com>
2021-12-08 00:16:14 +0900

    add props and classes

--- src/componets/ComponentViewCard.tsx
+++ src/componets/ComponentViewCard.tsx
@@ -4,13 +4,16 @@ import './component-view-card.css';
 type ComponentViewCardProps = {
   children: ReactElement,
   rounded?: boolean,
+  row?: 1 | 2 | 3 | 4;
+  col?: 1 | 2 | 3 | 4;
 }
 
 const ComponentViewCard: React.VFC<ComponentViewCardProps> = ({
-  children, rounded,
+  children, rounded, row = 1, col = 1,
 }) => {
   let classes = 'component-view-card ';
-  classes += rounded ? 'component-view-card--rounded' : '';
+  classes += rounded ? 'component-view-card--rounded ' : '';
+  classes += `component-view-card--row-${row} component-view-card--col-${col} `;
   return (
     <div className={classes}>
       <div className="component-view-card--view-component">
