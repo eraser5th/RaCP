fdd3fc608507a97e0b184a67a3f10b8ccafe65b2
eraser5th <hisanniwo0202@gmail.com>
2021-12-08 10:10:44 +0900

    import css and change

--- src/componets/Heading.tsx
+++ src/componets/Heading.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './heading.css';
 
 type HeadingProps = {
     text: string;
@@ -6,12 +7,12 @@ type HeadingProps = {
 };
 
 const Heading: React.FC<HeadingProps> = ({ text, level }) => {
-  if (level === 1) return <h1 className="level1-heading">{text}</h1>;
-  if (level === 2) return <h2 className="level2-heading">{text}</h2>;
-  if (level === 3) return <h3 className="level3-heading">{text}</h3>;
-  if (level === 4) return <h4 className="level4-heading">{text}</h4>;
-  if (level === 5) return <h5 className="level5-heading">{text}</h5>;
-  return <h6 className="level6-heading">{text}</h6>;
+  if (level === 1) return <h1 className="level1-heading"><span className="level1-heading__inner">{text}</span></h1>;
+  if (level === 2) return <h2 className="level2-heading"><span className="level2-heading__inner">{text}</span></h2>;
+  if (level === 3) return <h3 className="level3-heading"><span className="level3-heading__inner">{text}</span></h3>;
+  if (level === 4) return <h4 className="level4-heading"><span className="level4-heading__inner">{text}</span></h4>;
+  if (level === 5) return <h5 className="level5-heading"><span className="level5-heading__inner">{text}</span></h5>;
+  return <h6 className="level6-heading"><span className="level6-heading__inner">{text}</span></h6>;
 };
 
 export default Heading;
